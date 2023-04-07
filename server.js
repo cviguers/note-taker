cfd1bf2be72e2343d758b8f4fb2623c7e74de8bb
cviguers <cviguers@gmail.com>
2023-04-07 14:44:13 -0400

    api

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const noteData = require('./db/db.json');
+
 
 const PORT = 3001;
 
@@ -23,7 +25,40 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET route for api
+app.get('/api/notes', (req, res) => {
+  console.info(`GET /api/reviews`);
+  res.status(200).json(noteData);
+});
 
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { text, title } = req.body;
+
+  // If all the required properties are present
+  if (text && title) {
+    // Variable for the object we will save
+    const newNote = {
+      text,
+      title,
+      review_id: uuid(),
+    };
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error is posting, check to make sure you included both text and title');
+  }
+});
 
 
 app.listen(PORT, () =>

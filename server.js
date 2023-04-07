90514ab02ad378e0b708eefca7b950ed075c6288
cviguers <cviguers@gmail.com>
2023-04-07 15:11:58 -0400

    db routes

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET route for all clicks
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 // GET route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))

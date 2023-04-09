85c96d7662df1078e57360526baff6386e33b4b4
cviguers <cviguers@gmail.com>
2023-04-09 17:21:38 -0400

    routes

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
+//// server configuration
 // import needed packages and files
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteData = require('./db/db.json');
 
+// specify local and Heroku port
+const PORT = process.env.PORT || 3001;
 
-const PORT = 3001;
-
+// create express instance
 const app = express();
 
 // middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// static middleware pointing to public folder
 app.use(express.static('public'));
+
+// sets router to path
+app.use('/api', notesRouter);
+app.use('/', indexRouter);
 
 // GET route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//// routes
 // GET route for all clicks
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -32,7 +40,7 @@ app.get('/notes', (req, res) =>
 
 // GET route for api
 app.get('/api/notes', (req, res) => {
-  console.info(`GET /api/reviews`);
+  console.info(`GET ./api/reviews`);
   res.status(200).json(noteData);
 });
 
@@ -65,10 +73,8 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
-
+//// server start up and listening
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
-// GET/api/notes reads db.json => saved notes as JSON
-// POST/api/notes(new note)uuid!
